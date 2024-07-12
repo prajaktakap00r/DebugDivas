b2e67c875c8f2c4479050c788cd0fa01ac99d3cd
Yoshita Shaw <yoshitashaw@gmail.com>
2024-07-13 01:38:52 +0530

    Login.js Updated

--- myntra-clone/src/Components/Video.js
+++ myntra-clone/src/Components/Video.js
@@ -1,28 +1,66 @@
-// Video.js
-import React from 'react';
+import React, { useRef } from 'react';
 import '../App.css';
-import ReactDOM from 'react-dom';
 
 function Video(props) {
+    const videoRef = useRef(null);
+
     const handleClick = (e) => {
         e.preventDefault();
         e.target.muted = !e.target.muted;
     };
 
     const handleScroll = (e) => {
-        let next = ReactDOM.findDOMNode(e.target).parentNode.nextSibling;
-        if (next) {
-            next.scrollIntoView();
-            e.target.muted = true;
+        const videoElement = videoRef.current;
+        if (videoElement) {
+            const next = videoElement.parentNode.nextSibling;
+            if (next) {
+                next.scrollIntoView();
+                e.target.muted = true;
+            }
         }
     };
 
     return (
-        <video src={props.src} onEnded={handleScroll} className="videos-styling" id={props.id} onClick={handleClick} muted="muted"></video>
+        <video 
+            ref={videoRef} 
+            src={props.src} 
+            onEnded={handleScroll} 
+            className="videos-styling" 
+            id={props.id} 
+            onClick={handleClick} 
+            muted
+        ></video>
     );
 }
 
 export default Video;
+
+
+// // Video.js
+// import React from 'react';
+// import '../App.css';
+// import ReactDOM from 'react-dom';
+
+// function Video(props) {
+//     const handleClick = (e) => {
+//         e.preventDefault();
+//         e.target.muted = !e.target.muted;
+//     };
+
+//     const handleScroll = (e) => {
+//         let next = ReactDOM.findDOMNode(e.target).parentNode.nextSibling;
+//         if (next) {
+//             next.scrollIntoView();
+//             e.target.muted = true;
+//         }
+//     };
+
+//     return (
+//         <video src={props.src} onEnded={handleScroll} className="videos-styling" id={props.id} onClick={handleClick} muted="muted"></video>
+//     );
+// }
+
+// export default Video;
 
 
 // import React from 'react'
